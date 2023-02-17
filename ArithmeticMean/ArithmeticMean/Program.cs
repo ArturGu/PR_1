865f0d6a5fd9a6b4175eec8b67d93517d8afba69
Artur <gutnikartur3@gmail.com>
2023-02-17 23:52:48 +0200

    Добавьте файлы проекта.

---
+++ ArithmeticMean/ArithmeticMean/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArithmeticMean
+{
+    class Program
+    {
+        static void AerageValue(int a, int b, int c)
+        {
+            Console.WriteLine($"Вiдповiдь: " + ((a + b + c) / 3));
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть друге число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть третє число: ");
+            int c = int.Parse(Console.ReadLine());
+
+            AerageValue(a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR1_Calculator/PR1_Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PR1_Calculator
+{
+    public delegate int Delegat(int a, int b);
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введiть дiю (+,-,*,/): ");
+            string a = Console.ReadLine();
+            Console.Write("Перше число:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Друге число:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case "+":
+                    Delegat DelegatAdd = (c, b) => { return c + b; };
+                    int add = DelegatAdd(x, y);
+                    Console.WriteLine("Вiдповiдь:{0}", add);
+                    break;
+                case "-":
+                    Delegat DelegatSub = (c, b) => { return c - b; };
+                    int sub = DelegatSub(x, y);
+                    Console.WriteLine("Вiдповiдь:{0}", sub);
+                    break;
+                case "*":
+                    Delegat DelegatMul = (c, b) => { return c * b; };
+                    int mul = DelegatMul(x, y);
+                    Console.WriteLine("Вiдповiдь:{0}", mul);
+                    break;
+                case "/":
+                    Delegat DelegatDiv = (c, b) => { return c / b; };
+                    int div = DelegatDiv(x, y);
+                    Console.WriteLine("Вiдповiдь:{0}", div);
+                    break;
+                default:
+                    Console.WriteLine("Вкажіть вірну дію: (+,-,*,/)");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
