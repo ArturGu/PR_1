aae05238c84d21ad00cb7dda28eb97a2a54f4cce
ArturGu <88343962+ArturGu@users.noreply.github.com>
2023-05-17 17:57:30 +0300

    Add files via upload

---
+++ AnonymousMethod/AnonymousMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AnonymousMethod
+{
+    class Program
+    {
+        delegate int RandomDelegate();
+
+        static void Main(string[] args)
+        {
+            RandomDelegate[] delegates = new RandomDelegate[5];
+           
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                delegates[i] = () => new Random().Next(1, 100);
+            }
+
+            double average = CalculateAverage(delegates);
+
+            Console.WriteLine("Середнє арифметичне: " + average);
+
+            Console.ReadKey();
+        }
+
+        static double CalculateAverage(RandomDelegate[] delegates)
+        {
+            int sum = 0;
+
+            foreach (var del in delegates)
+            {
+                sum += del();
+            }
+           
+            double average = (double)sum / delegates.Length;
+
+            return average;
+        }
+    }
+}
